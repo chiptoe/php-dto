7bbc1a1cee4c1bbc9f1f3fcf8130b1aa73a62ccd
Your Name <you@example.com>
2025-08-08 18:48:26 +0200

    DTOAssocGenerator

---
+++ src/Generator/DTOAssocGenerator.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace Project\Generator;
+
+class DTOAssocGenerator
+{
+    /**
+     * @param array<string,mixed> $inputData
+     */
+    public function generate(array $inputData): string
+    {
+        return 'hello';
+    }
+}
