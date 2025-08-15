52cdbbffcf186d4aa0559fa4e1e5690b1b6ae073
Your Name <you@example.com>
2025-08-15 19:49:55 +0200

    add invalid dtoName test

--- tests/Generator/DTOAssocGeneratorTest.php
+++ tests/Generator/DTOAssocGeneratorTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\Generator;
 
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 use Project\DTOConverter\Utils;
 use Project\Generator\DTOAssocGenerator;
@@ -27,5 +28,46 @@ final class DTOAssocGeneratorTest extends TestCase
             file_get_contents(__DIR__ . '/TopicDTOAssoc.expected.txt'),
             $generated
         );
+    }
+
+    public static function provider_it_must_throw_if_dtoname_is_not_valid()
+    {
+        return [
+            [
+                'inputData' => [],
+                'expectedMessage' => 'the (dtoName) must exist as array key',
+            ],
+            [
+                'inputData' => [
+                    'dtoName' => 3
+                ],
+                'expectedMessage' => 'the (dtoName) must be string and filled',
+            ],
+            [
+                'inputData' => [
+                    'dtoName' => ''
+                ],
+                'expectedMessage' => 'the (dtoName) must be string and filled',
+            ],
+            [
+                'inputData' => [
+                    'dtoName' => '    '
+                ],
+                'expectedMessage' => 'the (dtoName) must be string and filled',
+            ],
+        ];
+    }
+
+    #[DataProvider('provider_it_must_throw_if_dtoname_is_not_valid')]
+    public function test_it_must_throw_if_dtoname_is_not_valid(
+        mixed $inputData,
+        mixed $expectedMessage,
+    )
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage($expectedMessage);
+
+        $service = new DTOAssocGenerator(new Utils());
+        $service->generate($inputData);
     }
 }
