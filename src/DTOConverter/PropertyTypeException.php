b85dc96fe39e32cf66bed6d0ba4cfde390443e9b
Your Name <you@example.com>
2025-08-17 22:32:43 +0200

    subKey

--- src/DTOConverter/PropertyTypeException.php
+++ src/DTOConverter/PropertyTypeException.php
@@ -7,7 +7,7 @@ class PropertyTypeException extends BaseException
 {
     public function __construct(
         public readonly string $invalidPropertyName,
-        public readonly string $invalidPropertyIndex,
+        public readonly string $subKey,
         \Throwable|null $previous = null,
         string $message = '',
     ) {
