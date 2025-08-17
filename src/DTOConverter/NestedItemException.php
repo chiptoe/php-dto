07929bfd35b31acd973877b6936be1af0b0781a3
Your Name <you@example.com>
2025-08-17 22:38:26 +0200

    NestedItemException

---
+++ src/DTOConverter/NestedItemException.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace Project\DTOConverter;
+
+class NestedItemException extends BaseException
+{
+    public function __construct(
+        public readonly string $invalidPropertyName,
+        public readonly string $nestedKey,
+        \Throwable|null $previous = null,
+        string $message = '',
+    ) {
+        if ($message === '') {
+            $message = 'Invalid nested item (' . $invalidPropertyName . ':' . $nestedKey . ').';
+        }
+
+        parent::__construct($message, 0, $previous);
+    }
+}
