05c6a1f98fe62ffaea79b25bf53b0fde2b29d37b
Your Name <you@example.com>
2025-08-17 20:27:34 +0200

    CommentDTO

---
+++ tests/DTO/CommentDTO/CommentDTO.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\DTO\CommentDTO;
+
+use Project\Exceptions\AccessToUninitialisedPropertyException;
+use Project\ValueObject\PositiveInt;
+use Project\ValueObject\PositiveIntNullable;
+
+final class CommentDTO
+{
+    private PositiveInt $id;
+
+    private PositiveIntNullable $parentId;
+
+    public function getId(): PositiveInt
+    {
+        if (!isset($this->id)) {
+            throw new AccessToUninitialisedPropertyException();
+        }
+
+        return $this->id;
+    }
+
+    public function setId(PositiveInt $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getParentId(): PositiveIntNullable
+    {
+        if (!isset($this->parentId)) {
+            throw new AccessToUninitialisedPropertyException();
+        }
+
+        return $this->parentId;
+    }
+
+    public function setParentId(PositiveIntNullable $parentId): self
+    {
+        $this->parentId = $parentId;
+
+        return $this;
+    }
+}
