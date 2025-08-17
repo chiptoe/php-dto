ab4cd2b0cbbf249e5ec0a623aab2be456d85d62c
Your Name <you@example.com>
2025-08-17 20:31:41 +0200

    comment dto

---
+++ tests/DTO/CommentDTO/CommentDTOConverter.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\DTO\CommentDTO;
+
+use Project\DTOConverter\PropertyTypeException;
+use Project\DTOConverter\PropertyTypeListException;
+use Project\DTOConverter\Utils;
+use Project\ValueObject\PositiveInt;
+use Project\ValueObject\PositiveIntNullable;
+
+final class CommentDTOConverter
+{
+    public function __construct(
+        private Utils $utils
+    ) {}
+
+    public function convert(mixed $inputData): CommentDTO
+    {
+        $this->utils->checkInputData(CommentDTOAssoc::getKeys(), $inputData);
+
+        $e = new PropertyTypeListException();
+        try {
+            $id = new PositiveInt($inputData[CommentDTOAssoc::ID]);
+        } catch (\Throwable $th) {
+            $e->add(new PropertyTypeException(CommentDTOAssoc::ID, $th));
+        }
+
+        try {
+            $parentId = new PositiveIntNullable($inputData[CommentDTOAssoc::PARENT_ID]);
+        } catch (\Throwable $th) {
+            $e->add(new PropertyTypeException(CommentDTOAssoc::PARENT_ID, $th));
+        }
+
+        if ($e->hasSomeExceptions()) {
+            throw $e;
+        }
+
+        return (new CommentDTO())
+            ->setId($id)
+            ->setParentId($parentId);
+    }
+}
