dd8c034b09d0e4d2aa37c6a2aa6af83ebe9590d5
Your Name <you@example.com>
2025-08-18 20:24:32 +0200

    convert commentRoot

--- tests/DTO/TopicDTO/TopicDTOConverter.php
+++ tests/DTO/TopicDTO/TopicDTOConverter.php
@@ -45,6 +45,12 @@ final class TopicDTOConverter implements IConverter
             $e->add(new PropertyTypeException(TopicDTOAssoc::COMMENTS, $th));
         }
 
+        try {
+            $commentRoot = $this->commentDTOConverter->convert($inputData[TopicDTOAssoc::COMMENT_ROOT]);
+        } catch (\Throwable $th) {
+            $e->add(new PropertyTypeException(TopicDTOAssoc::COMMENT_ROOT, $th));
+        }
+
         if ($e->hasSomeExceptions()) {
             throw $e;
         }
