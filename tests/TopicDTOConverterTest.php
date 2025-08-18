13fd4a57949ad658808259f5dbf8f2ad8c28b51f
Your Name <you@example.com>
2025-08-18 19:38:12 +0200

    test_it_must_throw_if_some_comments_item_is_invalid

--- tests/TopicDTOConverterTest.php
+++ tests/TopicDTOConverterTest.php
@@ -235,6 +235,15 @@ final class TopicDTOConverterTest extends TestCase
     }
 
     public function test_it_must_throw_if_some_comments_item_is_invalid() {
-        
+        $inputData = [
+            'id' => 3,
+            'parentId' => null,
+            'comments' => [
+                [
+                    'id' => 6,
+                    'parentId' => 7,
+                ],
+            ],
+        ];
     }
 }
