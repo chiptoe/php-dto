f8316622450faf7867461f43adcdb0d1a6068acd
Your Name <you@example.com>
2025-08-20 19:20:32 +0200

    update TopicDTOAssoc test

--- tests/Generator/DTOAssocGeneratorTest.php
+++ tests/Generator/DTOAssocGeneratorTest.php
@@ -18,6 +18,8 @@ final class DTOAssocGeneratorTest extends TestCase
             'fromKeys' => [
                 'id',
                 'parentId',
+                'comments',
+                'commentRoot',
             ],
         ];
 
