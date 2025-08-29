0e74c9fab67b1eaacb9d03b29efcfdc6d194b721
Your Name <you@example.com>
2025-08-29 10:45:13 +0200

    sync expected

---
+++ sync-expected.bash
@@ -0,0 +1,7 @@
+
+
+cp tests/DTO/TopicDTO/TopicDTOConverter.php tests/Generator/TopicDTOConverter.expected.txt
+
+cp tests/DTO/TopicDTO/TopicDTOAssoc.php tests/Generator/TopicDTOAssoc.expected.txt
+
+cp tests/DTO/TopicDTO/TopicDTO.php tests/Generator/TopicDTO.expected.txt
