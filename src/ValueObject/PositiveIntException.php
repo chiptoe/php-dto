2d327c2963a5221480120d386a37f8a038f57078
Your Name <you@example.com>
2025-09-02 19:08:17 +0200

    isTooBig

--- src/ValueObject/PositiveIntException.php
+++ src/ValueObject/PositiveIntException.php
@@ -9,11 +9,16 @@ class PositiveIntException extends \Exception
 {
     public function __construct(
         private int $max,
+        private bool $isTooBig,
         string $message = '',
         \Throwable|null $previous = null,
     ) {
         if ($message === '') {
-            $message = 'The (value) must be valid (' . PositiveInt::class . ').';
+            if ($this->isTooBig) {
+                $message = 'The (value) is too big, maximum allowed is ' . $this->max . ' (' . PositiveInt::class . ').';
+            } else {
+                $message = 'The (value) must be valid (' . PositiveInt::class . ').';
+            }
         }
 
         parent::__construct($message, 0, $previous);
@@ -22,5 +27,10 @@ class PositiveIntException extends \Exception
     public function getMax(): int
     {
         return $this->max;
+    }
+
+    public function isIsTooBig(): bool
+    {
+        return $this->isTooBig;
     }
 }
