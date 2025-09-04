e8862a74c09aa88dc228153b5d1c8e3e0490b072
Your Name <you@example.com>
2025-09-04 20:23:20 +0200

    StringUtils

--- src/Utils/StringUtils.php
+++ src/Utils/StringUtils.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace Project\Utils;
 
-final class StringCoreUtils
+final class StringUtils
 {
     public static function isNotStringOrBlank(mixed $value): bool
     {
